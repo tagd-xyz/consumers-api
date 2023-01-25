f50f08952cf57795761ae45199b9471c14ae7771
Juan Sierra <juan@totally.group>
2023-01-25 16:53:18 +0100

    Initial commit

--- app/Http/V1/Resources/Item/Tagd/Single.php
+++ app/Http/V1/Resources/Item/Tagd/Single.php
@@ -3,6 +3,7 @@
 namespace App\Http\V1\Resources\Item\Tagd;
 
 use App\Http\V1\Resources\Actor\Consumer\SingleBrief as ConsumerSingle;
+use App\Http\V1\Resources\Actor\Reseller\SingleBrief as ResellerSingle;
 use App\Http\V1\Resources\Item\Item\Single as ItemSingle;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -19,8 +20,10 @@ class Single extends JsonResource
         return [
             'id' => $this->id,
             'slug' => $this->slug,
+            'isRoot' => $this->is_root,
             // TODO: 'consumer' => new ConsumerSingle($this->whenLoaded('consumer')),
             'consumer' => new ConsumerSingle($this->consumer),
+            'reseller' => new ResellerSingle($this->reseller),
             'item' => new ItemSingle($this->whenLoaded('item')),
             'meta' => $this->meta,
             'createdAt' => $this->created_at,
